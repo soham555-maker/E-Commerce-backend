78198dd33fa0bc69701e5211bfd94742f07f2f74
Soham Sunil Margaj <sohammargaj55555@gmail.com>
2024-02-01 23:34:57 +0530

    blabla

--- Middelware/uploadImg.js
+++ Middelware/uploadImg.js
@@ -4,7 +4,7 @@ import path from "path";
 const storage = multer.diskStorage({
     destination: './uploads/images',
     filename:(req, file, cb)=>{
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+        return cb(null, `${file.originalname}_${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
     }
 })
 export const upload = multer({storage:storage});
---
+++ app.js
@@ -0,0 +1,37 @@
+import uploadRouter from './Routes/uploadImgRoute.js';
+import getAllProductsRouter from './Routes/getAllProductsRoute.js';
+import usersRouter from './Routes/usersRoute.js';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import cartFunctionsRouter from './Routes/cartFunctionsRoute.js'
+import env from 'dotenv'
+env.config()
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(cors());
+
+
+// Database Connection With MongoDB
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{console.log("mongodb connected.")});
+
+// Connecting Routes:
+app.use("/", uploadRouter);
+app.use("/", getAllProductsRouter);
+app.use("/", usersRouter);
+app.use("/", cartFunctionsRouter);
+
+
+// API Creation
+app.listen(port, (error) => {
+  if (!error) {
+    console.log("Server Running On Port " + port);
+  }
+});
